3a0ed5052cd54f957f5830d25e1a18b2f3fe2057
RooRsmoka <chri.disa96@gmail.com>
2022-02-14 23:15:28 +0100

    created php variables for the censorship of a word chosen by the user

--- index.php
+++ index.php
@@ -4,7 +4,9 @@ $myText = "Benvenuto in questa pagina PHP.";
 // Stampare a schermo il paragrafo e la sua lunghezza.
 // var_dump($myText);
 // Una parola da censurare viene passata dall’utente tramite parametro GET.
+$censoredWord = $_GET["word"];
 // Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+$censoredText = str_replace($censoredWord, "***", $myText);
 ?>
 
 <!DOCTYPE html>
@@ -18,5 +20,8 @@ $myText = "Benvenuto in questa pagina PHP.";
     <body>
         <p><?php echo $myText?></p>
         <p>Il testo è lungo <?php echo strlen($myText)?> caratteri.</p>
+        </br>
+        <p><?php echo $censoredText?></p>
+        <p>il testo è lungo <?php echo strlen($censoredText)?> caratteri.</p>
     </body>
 </html>
